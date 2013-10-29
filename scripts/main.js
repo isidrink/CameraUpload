436b9ad7addc43c7a4800e0ae1ea5c321b648f0b
isidrink <isidro.escoda@gmail.com>
2013-10-29 19:22:55 +0100

    Prueba23

--- scripts/main.js
+++ scripts/main.js
@@ -15,10 +15,36 @@ require.config({
         }
     }
 });
-
+            
+           
 require(["jQuery", "app"], function($, application) {
     $(function() {
         app = application
         application.init();
     });
 });
+
+$(document).ready(function(){
+            $(document).bind('deviceready', function(){
+                    var output = $('#output');
+
+                    $.ajax({
+                            url: 'http://samcroft.co.uk/demos/updated-load-data-into-phonegap/landmarks.php',
+                            dataType: 'jsonp',
+                            jsonp: 'jsoncallback',
+                            timeout: 5000,
+                            success: function(data, status){
+                                    $.each(data, function(i,item){ 
+                                            var landmark = '<li><h1>'+item.name+'</h1>'
+                                            + '<p>'+item.latitude+'<br>'
+                                            + item.longitude+'</p></li>';
+
+                                            output.append(landmark);
+                                    });
+                            },
+                            error: function(){
+                               output.text('There was an error loading the data.');
+                            }
+                    });
+            });
+    });
